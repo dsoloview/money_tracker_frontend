50e086b11a6ba791c67ed69961aadb99556fa2ac
filaretovdmitriy <filaretovd@yandex.ru>
2024-04-09 12:04:07 +0300

    tanstack's pending

--- src/features/transaction/TransactionTableTools.tsx
+++ src/features/transaction/TransactionTableTools.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@chakra-ui/react";
 import { useDeleteTransaction } from "../../api/endpoints/transactions/transactions.api.ts";
-import { useState } from "react";
 import i18next from "../../tools/language/language.ts";
 type Props = {
   transactionId: number;
@@ -9,24 +8,20 @@ type Props = {
 export default function TransactionTableTools({
   transactionId,
 }: Props): JSX.Element {
-  const { mutate } = useDeleteTransaction();
-  const [pendingDelete, setPendingDelete] = useState(false);
+  const { mutate, isPending } = useDeleteTransaction();
   const handleTransactionDelete = async () => {
-    if (pendingDelete) return;
-    setPendingDelete(true);
-    try {
-      await mutate(transactionId);
-      setPendingDelete(false);
-    } catch (error) {
-      setPendingDelete(false);
-    }
     mutate(transactionId);
   };
 
   return (
     <>
-      <Button colorScheme="red" size="xs" onClick={handleTransactionDelete}>
-        {i18next.t('button.delete')}
+      <Button
+        disabled={isPending}
+        colorScheme="red"
+        size="xs"
+        onClick={handleTransactionDelete}
+      >
+        {i18next.t("button.delete")}
       </Button>
     </>
   );
